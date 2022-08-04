6c338b9efeccdbd9d22a2fa93d721db7af33c161
Jose Encarnacion <Jose.Encarnacion@dpworld.com.do>
2022-08-04 13:39:16 -0400

    first commit

---
+++ src/test/java/com/jprograming/restapitask/RestapitaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jprograming.restapitask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapitaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
